ae09d441a493f0a3b0096685ba8bd6f48d3de478
NuII <>
2019-04-13 18:14:30 -0500

    a
    a

--- CSGOSimple-master/CSGOSimple/fakelag.cpp
+++ CSGOSimple-master/CSGOSimple/fakelag.cpp
@@ -6,10 +6,10 @@
 
 void chris::features::fakelag::oncreatemove(CUserCmd* cmd)
 {
-	static int ticks = 0;
+	static int ticks = 0; // this entire part is basically cause clientstate is a fuck and wont display choked commands properly
 	static int maxticks = 16;
 
-	if (ticks >= maxticks)
+	if (ticks >= maxticks) // literal worst code you'll ever see but it werks
 	{
 		g_GlobalVars->sendpacket = true;
 		ticks = 0;
@@ -29,19 +29,19 @@ void chris::features::fakelag::oncreatemove(CUserCmd* cmd)
 
 		case 1: // factor
 		{
-			g_GlobalVars->sendpacket = (ticks >= g_Options.fakelag_ticks);
+			g_GlobalVars->sendpacket = (ticks >= g_Options.fakelag_ticks); // thank u stacker
 		}
 		break;
 
 		case 2: // adaptive
 		{
-			chris::features::fakelag::adaptive(ticks);
+			chris::features::fakelag::adaptive(ticks); // for some reason if I tried to call the adaptive as an int it didn't properly update
 		}
 		break;
 	}
 }
 
-void chris::features::fakelag::adaptive(int ticks)
+void chris::features::fakelag::adaptive(int ticks) // pasted from reversed aimware :^) reversed aimware pasted from chance :^))
 {
 	int wish_ticks = 0, adaptive_ticks = 2;
 	float extrapolated_speed = g_LocalPlayer->m_vecVelocity().Length2D() * g_GlobalVars->interval_per_tick;
